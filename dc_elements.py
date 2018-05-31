42fb71cba89f0c93870d7b5651c18cf9c2837baa
gtsmithuk <gtsmithuk@users.noreply.github.com>
2018-05-31 18:42:26 +0200

    testing_first_commit

---
+++ dc_elements.py
@@ -0,0 +1,3 @@
+#calculate equlibrium potential from Nernst Equation
+
+E = E0 - T * R/ F/ n * math.log(p_anode/p_cathode)
---
+++ params.py
@@ -0,0 +1,17 @@
+#operating constants
+T = 80 + 273.15 #K
+R = 8.314       #J K-1 M-1
+p_anode = 0.1   #MPa
+p_cathode = 100 #MPa
+current = 1     #A cm-2
+RH = 80         #%
+
+#cell constants
+E0 = 0          #Hydrogen on both sides, E0 for both sides 
+n = 2           #Electrons 
+
+#material constants
+mem_thickickness = 50 #cm
+
+#fundamental constants
+F = 96485       #C mol-1
